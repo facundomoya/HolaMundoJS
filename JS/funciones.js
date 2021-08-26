69563624cb410d8820a79e7071904877bd33b9c5
Facundo <facundomoya18@hotmail.com>
2021-08-26 11:34:19 -0300

    se avanzo en el tema funciones en le archivo funciones.js

--- JS/funciones.js
+++ JS/funciones.js
@@ -8,8 +8,23 @@ function holaMundo(){
 //funciones con parametros
 
 function saludar(nombre,apellido){
-    document.write("<br>Hola mi nombre es: "+nombre+"y mi apellido es: "+apellido)
+    document.write("<br>Hola mi nombre es: "+nombre+" y mi apellido es: "+apellido)
 }
 
-//ejecutar o invocar o llamar a una funcion
+//funciones que retornan un valor
+function convertirdolarespesos(dolares){
+    let pesos = dolares * 190;
+    return pesos;
+}
+
+//ejecutar o invocar o llamar a una funcion sin parametros
+
 holaMundo();
+
+//llamar una funcion con parametros
+
+saludar("Esteban","Marquez");
+
+let precioIphone = convertirdolarespesos(599)
+document.write("<br>Iphone SE $ "+precioIphone)
+document.write("<br>Jordan $ "+convertirdolarespesos(200))
