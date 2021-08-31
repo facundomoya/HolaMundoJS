1366eafc37a2eef7eda16a2825289e04b0b4021c
Facundo <facundomoya18@hotmail.com>
2021-08-31 11:36:29 -0300

    se creo el archivo objetos.js, y se comenzo con la teoria del tema

---
+++ JS/objetos.js
@@ -0,0 +1,13 @@
+let nombre = "Tony Stark"
+let numSerie = "123asd"
+let precio = 5000
+let version = "end-game"
+
+//crear un objeto con notacion literal
+let funkoIronMan = {
+    //clave: valor -> son propiedades
+    nombre: "Tony Stark",
+    numSerie: "123asd",
+    precio: 5000,
+    version: "end-game"
+}
