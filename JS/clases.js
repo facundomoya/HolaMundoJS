119365ff41797760634ac92ae84ba62c2f8c0c25
Facundo <facundomoya18@hotmail.com>
2021-09-09 11:39:17 -0300

    se finalizo el proyecto HolaMundo, habiendo visto la teoria completa de clases.js, aun faltan los ejercicios restantes del tp de objetos

--- JS/clases.js
+++ JS/clases.js
@@ -1,7 +1,7 @@
 //crear una clase
 class Funkopop{
-    //aqui comienzo a crear mi clase o molde
-    constructor(nombreFunko,precioFunko,version,numSerie){
+    //aqui comienzo a crear mi clase o molde el constructor es un METODO
+    constructor(nombreFunko,precioFunko,version,numSerie){ //este this reemplaza donde despues va a estar el objeto, cuando lo cree mas adelante en el futuro. En la notacion LITERAL no es necesario porque ya esta creado el objeto, en cambio cuando creo una clase, el objeto se crea como instancia y va mas adelante que la clase
         this.nombre = nombreFunko
         this.precio = precioFunko
         this.version = version
@@ -19,7 +19,7 @@ class Funkopop{
         <br>Disponible: ${this.disponible}`)
     }
 
-    //propiedades conmutadas get y set ---> GET = accede a una sola propiedad de un objeto
+    //propiedades conmutadas get y set ---> GET = accede a una sola propiedad de un objeto, por ejemplo muestra el nombre de una clase y no de todas
     get mostrarNombre(){
         return this.nombre
     }
@@ -34,31 +34,78 @@ class Funkopop{
 }
 
 class Persona{
-    constructor(nombre, apellido, dni, mail,telefono,edad){
-        this.nombre = nombre;
+    constructor(nombre, apellido, dni, mail,telefono,edad,usuario,pass){ 
+        this.nombre = nombre
         this.apellido = apellido;
         this.dni = dni;
         this.mail = mail;
         this.telefono = telefono;
         this.edad = edad;
+        this.usuario = usuario
+        this.pass = pass
     }
 
     mostrardatos(){
-        document.write(`
+        document.write(`<br>
         <br>Nombre: ${this.nombre}
         <br>Apellido: ${this.apellido}
         <br>DNI: ${this.dni}
         <br>Mail: ${this.mail}
         <br>Telefono: ${this.telefono}
         <br>Edad: ${this.edad}
+        <br>Usuario: ${this.usuario}
+        <br>Contraseña: ${this.pass}`)
+    }
+}
+
+//herencia
+
+class Alumno extends Persona{
+    constructor(nombre,apellido,dni,mail,telefono,edad,usuario,pass,matricula,comision,notas,curso){
+        // invocar el metodo constructor de la clase persona
+        super(nombre,apellido,dni,mail,telefono,edad,usuario,pass)
+        this.matricula = matricula;
+        this.comision = comision;
+        this.notas = notas;
+        this.curso = curso;
+    }
+    //sobreescribir metodos 
+    mostrardatos(){
+        document.write(`<br>
+        <br>Nombre: ${this.nombre}
+        <br>Apellido: ${this.apellido}
+        <br>DNI: ${this.dni}
+        <br>Mail: ${this.mail}
+        <br>Telefono: ${this.telefono}
+        <br>Edad: ${this.edad}
+        <br>Matricula: ${this.matricula}
+        <br>Comision: ${this.comision}
+        <br>Notas: ${this.notas}
+        <br>Curso: ${this.curso}`)
+    }
+    //todos los metodos que quiero crear, set y get
+}
+
+class Tutor extends Persona{
+    constructor(nombre,apellido,dni,mail,telefono,edad,usuario,pass,legajo,comision,antiguedad){
+        super(nombre,apellido,dni,mail,telefono,edad,usuario,pass)
+        //aqui va lo que agregamos a tutor lo que difiere de persona, eso es: legajo,comision,antiguedad
+        this.legajo = legajo
+        this.comision = comision
+        this.antiguedad = antiguedad
+    }
+
+    mostrardatostutor(){
+        document.write(`
+        <br>Legajo: ${this.legajo}<br>
+        Comision: ${this.comision}<br>
+        Antiguedad: ${this.antiguedad}<br>
         `)
     }
 }
 
 //¿que tiene un alumno que no tenga otras personas? notas, usuario, contraseña, matricula, comision
 //¿que tiene un tutor? usuario, contraseña, legajo, comision
-
-
 
 console.log("Prueba")
 
@@ -83,8 +130,15 @@ document.write("<br>Propiedad Precio nuevo: $"+strange.mostrarPrecio)
 
 
 //2 clase (Persona)
-let juan = new Persona("Juan","Alaniz",33484859,"jalaniz@gmail.com","156615007",35)
-let abel = new Persona("Abel","Cordoba",33484859,"abel@gmail.com","156500501",34)
+let juan = new Persona("Juan","Alaniz",33484859,"jalaniz@gmail.com","156615007",35,"jalainiz","contrase12")
+let abel = new Alumno("Abel","Cordoba",33484859,"abel@gmail.com","156500501",34,"cordoba567","password123",3435,"16i","A","Fullstack")
 
+//para juan nos muestra el mostrardatos() de la clase Persona
+//para abel nos muestra el mostrardatos() de la clase Alumno, alli se sobreescribio el metodo mostrardatos()
 juan.mostrardatos()
 abel.mostrardatos()
+
+let jony = new Tutor ("Jony","Plodzien",37828894,"jony@gmail.com",1555941123,30,"jony123","contraseña123",46280,"16i",2)
+
+jony.mostrardatos()
+jony.mostrardatostutor()
